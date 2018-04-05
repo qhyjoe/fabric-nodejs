9077ac1db1033a951bbda3cbe50a1b850bbf2579
qiaohongyu <397982217@qq.com>
2018-04-05 19:40:29 +0800

    2

--- routes/fabricCli.js
+++ routes/fabricCli.js
@@ -1,7 +1,27 @@
 var express = require('express');
 var router = express.Router();
-router.get('/', function(req, res, next) {
-    res.render('fabric', { title: 'Hello Fabric' });
+var url = require('url');
+var Client = require('fabric-client');
+var sdkUtils = require('fabric-client/lib/utils')
+router.get('/query', function (req, res, next) {
+    //res.render('fabric', { title: 'Hello Fabric' });
+    //console.log(req.url);
+    var id=req.query.id
+    console.log(id);
+    query(id);
 });
+function init() {
+    Client.addConfigFile(path.join(__dirname, './config.json'));
+    var ORGS = Client.getConfigSetting('network');
 
+};
+function query(arg){
+    'use strict';
+    init()
+}
+
+
+function invoke(arg){
+    'use strict';
+}
 module.exports = router;
