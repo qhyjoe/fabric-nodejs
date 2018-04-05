5be2f3f795ad0ccb860dfd2dbc2573abd0758501
qiaohongyu <397982217@qq.com>
2018-04-05 20:40:36 +0800

    1

--- routes/fabricCli.js
+++ routes/fabricCli.js
@@ -1,23 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var url = require('url');
+var path = require('path');
 var Client = require('fabric-client');
-var sdkUtils = require('fabric-client/lib/utils')
+var sdkUtils = require('fabric-client/lib/utils');
+var ORGS;
 router.get('/query', function (req, res, next) {
     //res.render('fabric', { title: 'Hello Fabric' });
     //console.log(req.url);
     var id=req.query.id
     console.log(id);
+    console.log(Client);
     query(id);
+    res.render('fabric', { title: JSON.stringify(ORGS) });
 });
 function init() {
+    console.log(path.join(__dirname, './config.json'));
     Client.addConfigFile(path.join(__dirname, './config.json'));
-    var ORGS = Client.getConfigSetting('network');
+    ORGS = Client.getConfigSetting('network');
+    console.log(ORGS)
 
 };
 function query(arg){
     'use strict';
-    init()
+    init();
+
 }
 
 
